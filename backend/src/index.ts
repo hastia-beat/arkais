ec6712e2c6b9633c202d8652f03b959f1683cfcc
Calmantara <calmantarasp@gmail.com>
2024-08-30 22:16:45 +0700

    feat: add collection feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,21 +23,58 @@ prisma
 // cors
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
+
+// json parser
+app.use(express.json());
 
 // words group
 app.get("/words", async (req, res) => {
   try {
-    const search = req.query.search;
-
+    const kata = req.query.kata ? req.query.kata.toString() : "";
     // construct query
     const query = {
-      kata: { contains: search ? search.toString() : "" },
+      kata: { contains: kata },
     };
 
     const words = await prisma.word.findMany({ where: query });
     res.status(200).send(words);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+app.post("/collections", async (req, res) => {
+  try {
+    const body = req.body;
+    const word = await prisma.collection.create({
+      data: {
+        wordId: body.wordId,
+        email: body.email,
+      },
+    });
+    res.status(201).send(word);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+app.get("/collections", async (req, res) => {
+  try {
+    const email = req.query.email ? req.query.email.toString() : "";
+    const collections = await prisma.collection.findMany({
+      where: {
+        email: email,
+      },
+    });
+    res.status(200).send(collections);
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: "Internal server error" });

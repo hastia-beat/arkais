2949834590d90da6d289ac94c81d0856b8cf8ed2
hastia-beat <limeyound@gmail.com>
2024-08-30 17:32:08 +0700

    user and searchterm data

--- src/app/components/collectionButton.js
+++ src/app/components/collectionButton.js
@@ -4,29 +4,32 @@ import React, { useState } from "react";
 import { BookmarksSimple } from '@phosphor-icons/react';
 
 const CollectionButton = ({ word_id, user_email }) => {
+  console.log(word_id, user_email)
 
-  const [isCreated, setIsCreated] = useState(false);
+  const [isCreated, setIsCreated] = useState(false)
 
   const handleCollection = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const data = { word_id, user_email };
 
     const response = await fetch("api/v1/collection", {
       method: "POST",
-      body: JSON.stringify(data),
-    });
-
-    const collection = await response.json();
+      body: JSON.stringify(data)
+    })
+    const collection = await response.json()
     if (collection.isCreated) {
       setIsCreated(true);
     }
   };
 
   return (
-    <button onClick={handleCollection}>
-      <BookmarksSimple size={28} />
-    </button>
+    <>
+      {isCreated && <p>Added to collection</p>}
+      <button onClick={handleCollection}>
+        <BookmarksSimple size={28} />
+      </button>
+    </>
   );
 };
 

1de4cdbdd0390c6607f18b1c51d7c0463062d56e
hastia-beat <limeyound@gmail.com>
2024-09-08 16:06:39 +0700

    last-last

--- frontend/src/app/api/collection/route.js
+++ frontend/src/app/api/collection/route.js
@@ -1,3 +1,6 @@
+// Mark this route as dynamic
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOption } from "@/app/api/auth/[...nextauth]/route";
@@ -11,7 +14,7 @@ export async function GET() {
             return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
         }
 
-        const collectionUsecase = new CollectionUsecase();
+        const collectionUsecase = new CollectionUsecase();  
         const collections = await collectionUsecase.fetchCollection(session.user.email);
 
         return NextResponse.json({ user: session.user, collections }, { status: 200 });
